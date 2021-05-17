2892690806da2d4468ad611fa3b51307168235b6
Fernanda Gaytán <fernandagaytan@MacBook-Air-de-Fernanda-2.local>
2021-05-16 22:35:47 -0500

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,12 @@
 import { useSelector } from 'react-redux';
 
-const translationData = {
-  ES: {
-    welcome: 'Bienvenido',
-    intro: 'Esta es una aplicacion de React.',
-  },
-  EN: {
-    welcome: 'Welcome',
-    intro: 'This is a React application.',
-  },
-};
-
 const Home = () => {
   const { selectedLang, defaultLang } = useSelector((state) => state);
-
+  console.log({ selectedLang, defaultLang });
   return (
     <div className='prose max-w-none container mx-auto flex flex-col px-5 py-12 justify-center items-center'>
-      {defaultLang && selectedLang === '' && (
-        <>
-          <h1>{translationData[defaultLang].welcome}</h1>
-          <p>{translationData[defaultLang].intro}</p>
-        </>
-      )}
-      {selectedLang !== '' && (
-        <>
-          <h1>{translationData[selectedLang].welcome}</h1>
-          <p>{translationData[selectedLang].intro}</p>
-        </>
-      )}
+      <h1>Welcome</h1>
+      <p>This is a React App</p>
     </div>
   );
 };
